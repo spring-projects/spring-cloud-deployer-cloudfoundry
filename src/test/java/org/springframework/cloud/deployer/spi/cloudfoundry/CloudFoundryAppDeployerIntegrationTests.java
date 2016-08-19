67bd1e661327005fb358015b525fee56b094f5fc
Eric Bottard <eric.bottard+github@gmail.com>
2016-08-19 13:13:37 +0200

    Use MavenProperties in TCK tests
    Fixes spring-cloud/spring-cloud-deployer-cloudfoundry#76

--- src/test/java/org/springframework/cloud/deployer/spi/cloudfoundry/CloudFoundryAppDeployerIntegrationTests.java
+++ src/test/java/org/springframework/cloud/deployer/spi/cloudfoundry/CloudFoundryAppDeployerIntegrationTests.java
@@ -16,17 +16,27 @@
 
 package org.springframework.cloud.deployer.spi.cloudfoundry;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Ignore;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.deployer.resource.maven.MavenProperties;
+import org.springframework.cloud.deployer.resource.maven.MavenResource;
 import org.springframework.cloud.deployer.spi.app.AppDeployer;
 import org.springframework.cloud.deployer.spi.test.AbstractAppDeployerIntegrationTests;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 /**
  * Integration tests for CloudFoundryAppDeployer.
@@ -48,6 +58,9 @@ public class CloudFoundryAppDeployerIntegrationTests extends AbstractAppDeployer
 	protected AppDeployer appDeployer() {
 		return appDeployer;
 	}
+
+	@Autowired
+	private MavenProperties mavenProperties;
 
 	/**
 	 * Execution environments may override this default value to have tests wait longer for a deployment, for example if
@@ -94,8 +107,35 @@ public class CloudFoundryAppDeployerIntegrationTests extends AbstractAppDeployer
 	 */
 	@Configuration
 	@EnableAutoConfiguration
+	@EnableConfigurationProperties
 	public static class Config {
+
+		@Bean
+		@ConfigurationProperties("maven")
+		public MavenProperties mavenProperties() {
+			return new MavenProperties();
+		}
+
 
 	}
 
+	// Overridden here for imminent release until
+	// https://github.com/spring-cloud/spring-cloud-deployer/issues/102 is fixed
+	@Override
+	protected Resource integrationTestProcessor() {
+		Properties properties = new Properties();
+		try {
+			properties.load(new ClassPathResource("integration-test-app.properties").getInputStream());
+		}
+		catch (IOException e) {
+			throw new RuntimeException("Failed to determine which version of integration-test-app to use", e);
+		}
+		return new MavenResource.Builder(mavenProperties)
+				.groupId("org.springframework.cloud")
+				.artifactId("spring-cloud-deployer-spi-test-app")
+				.classifier("exec")
+				.version(properties.getProperty("version"))
+				.extension("jar")
+				.build();
+	}
 }
